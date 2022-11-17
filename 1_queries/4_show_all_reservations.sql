1cd5f702b620c6d2d04961df28bf5b62e4e95ddc
Cheryet <C.heryet@outlook.com>
2022-11-16 18:23:24 -0700

    downloaded required files

--- 1_queries/4_show_all_reservations.sql
+++ 1_queries/4_show_all_reservations.sql
@@ -5,4 +5,4 @@ JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, cost_per_night
 ORDER BY start_date
-LIMIT 10
+LIMIT 10;
